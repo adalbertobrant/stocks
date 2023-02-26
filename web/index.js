1c5fbbe3330a1f8a83cbe6b15789c48aba96a3f6
adalbertobrant <adalbertobrant@gmail.com>
2023-02-26 17:37:48 -0300

    new files for nodejs server

---
+++ web/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+app.listen(3000, function() {
+  console.log('Server started on port 3000');
+});
