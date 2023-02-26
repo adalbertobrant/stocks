a1e6a241377dda80a225df4476d74df920f8d116
adalbertobrant <adalbertobrant@gmail.com>
2023-02-26 18:38:44 -0300

    porta modificada

--- web/index.js
+++ web/index.js
@@ -11,6 +11,6 @@ app.get('/', function(req, res) {
   res.sendFile(__dirname + '/public/index.html');
 });
 
-app.listen(3000, function() {
+app.listen(5000, function() {
   console.log('Server started on port 3000');
 });
